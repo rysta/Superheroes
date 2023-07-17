6030f29017c5f17b9e345213589c3c223735e91c
rysta <rysta@mail.ru>
2023-07-17 10:02:47 +0300

    +

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -1,35 +1,53 @@
 package com.example.superheroes
 
 import HeroesRepository
+import android.graphics.drawable.shapes.Shape
+import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.superheroes.model.Hero
 
 @Composable
 fun HeroItem(hero: Hero, modifier: Modifier = Modifier){
     Card(
-        modifier = modifier
+        elevation = CardDefaults
+            .cardElevation(
+                defaultElevation = dimensionResource(R.dimen.card_elevation)
+            ),
+        modifier = modifier.clip(RoundedCornerShape(dimensionResource(R.dimen.card_clip)))
     ) {
         Row(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(dimensionResource(R.dimen.padding_medium))
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium))
+                .sizeIn(minHeight = dimensionResource(R.dimen.card_size))
         ) {
             HeroInformation(hero, modifier)
+            Spacer(
+                modifier = modifier.width(dimensionResource(R.dimen.between_text_image_spacer))
+            )
             HeroImage(hero, modifier)
         }
     }
@@ -55,12 +73,9 @@ fun HeroInformation(hero: Hero, modifier: Modifier = Modifier) {
 fun HeroImage(hero: Hero, modifier: Modifier = Modifier) {
     Image(
         painter = painterResource(hero.imageRes),
-        contentDescription = stringResource(hero.descriptionRes)
+        contentDescription = stringResource(hero.descriptionRes),
+        modifier = modifier
+            .size(dimensionResource(R.dimen.image_size))
+            .clip(MaterialTheme.shapes.small)
     )
-}
-
-@Preview
-@Composable
-fun HeroItemPreview(){
-    HeroItem(hero = HeroesRepository.heroes.component1())
 }
--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -22,25 +22,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SuperheroesTheme {
-        Greeting("Android")
+        HeroItem(hero = HeroesRepository.heroes.component1())
     }
 }
