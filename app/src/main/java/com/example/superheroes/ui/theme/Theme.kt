42b0936656aa14c9ed111146a46c01e62ed7d6c3
rysta <rysta@mail.ru>
2023-07-21 09:19:31 +0300

    edge-to-edge

--- app/src/main/java/com/example/superheroes/ui/theme/Theme.kt
+++ app/src/main/java/com/example/superheroes/ui/theme/Theme.kt
@@ -98,7 +98,7 @@ fun SuperheroesTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
